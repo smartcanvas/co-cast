cefe4782c5bb35c053f9aec8ecf398affb86f60f
Daniel Viveiros <daniel.viveiros@gmail.com>
2015-04-17 20:11:04 -0300

    Creating all pages

--- src/main/webapp/js/c2.js
+++ src/main/webapp/js/c2.js
@@ -25,13 +25,13 @@ window.addEventListener('polymer-ready', function(e) {
     if (this.response != null) {
       var cardList = "";
       for (i = 0; i < this.response.length; i++ ) {
-        cardList += "<casted-card contentTitle='" + this.response[i].title + "' "
+        cardList += "<div><casted-card contentTitle='" + this.response[i].title + "' "
             + "content='" + this.response[i].content + "' "
-            + "authorImageURL='" + this.response[i].authorImageURL + "' "
+            + "authorImageURL='" + resizeAuthorImage(this.response[i].authorImageURL) + "' "
             + "imageURL='" + this.response[i].contentImageURL + "' "
             + "authorName='" + this.response[i].authorDisplayName + "' "
-            + "date='" + this.response[i].date + "' "
-            + "></casted-card>";
+            + "date='" + formatDate(this.response[i].date) + "' "
+            + "></casted-card></div>";
       }
       document.getElementById("casted-card-list").innerHTML = cardList;
     }
@@ -39,3 +39,34 @@ window.addEventListener('polymer-ready', function(e) {
 
   ajax.go(); // Call its API methods.
 });
+
+/**
+ * Resizes the author imageURL
+ */
+function resizeAuthorImage(authorImageURL) {
+  indexOfQuestionMark = authorImageURL.indexOf('?');
+  originalURL = authorImageURL.substring(0,indexOfQuestionMark);
+  originalURL += "?sz=150";
+  return originalURL;
+}
+
+/**
+ * Format the date in a string 
+ */
+function formatDate(dateInUTC) {
+
+  var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+  ];
+
+  var date = new Date( dateInUTC );
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return monthNames[monthIndex] + " " + day + ", " + year;
+
+}
