5f502c8b837302eae4f8c27a941efb2f12b9f6e7
Adriano Falsarella Monte <adrifmonte@gmail.com>
2016-02-22 16:18:02 -0300

    smooth update

--- app/scripts/carousel.js
+++ app/scripts/carousel.js
@@ -13,21 +13,31 @@
 
   function Carousel(itemsLength, loadNextCallback){
     this.itemsLength = itemsLength - 1;
+    this.lastPosition = -1;
     this.atualPosition = 0;
     this.restarting = false;
+    this.offset = 0;
 
     this.loadNext = function(){
       if(loadNextCallback){
-        loadNextCallback(this.atualPosition, this.restarting);  
+        loadNextCallback(this.atualPosition, this.restarting, this.lastPosition);  
       }
+      this.lastPosition = this.atualPosition;
 
-      if(this.itemsLength === this.atualPosition){
+      if(this.itemsLength === this.atualPosition || this.atualPosition > this.itemsLength){
         this.restarting = true;
-        this.atualPosition = 0;
+        this.atualPosition = this.offset;
       }else{
         this.restarting = false;
         this.atualPosition++;
       }
+    };
+
+    this.reset = function(newItemsLength){
+      this.atualPosition = this.itemsLength + this.atualPosition - this.offset + 1;
+      this.offset = this.itemsLength + 1;
+      this.itemsLength = newItemsLength - 1;
+      this.restarting = true;
     };
 
     this.loadNext();
